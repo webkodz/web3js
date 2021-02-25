1d6c1bf836c92305e8a59fce5f40ed8ef91105e9
Lavish Patel <lavish.webkodz@gmail.com>
2021-02-25 20:20:45 +0530

    commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const Web3 = require('web3')
+const Tx = require('ethereumjs-tx')
+const rpcURL = "https://mainnet.infura.io/v3/YOUR-PROJECT-ID"
+
+// connect to Infura node
+const web3 = new Web3(new Web3.providers.HttpProvider(rpcURL))
+
+web3.eth.getBalance("Account_Address", function (err, result) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(web3.utils.fromWei(result, "ether") + " ETH")
+    }
+})
+
+// EX- Expected Output 
+// 5110.694355 ETH
